47fbbe50e854382618f2b21cc765b435ac3ab6e5
iionly <iionly@gmx.de>
2013-11-11 19:51:09 +0100

    Initial release for Elgg 1.9; see README.txt

---
+++ jpatchwork/languages/de.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This is a proof-of-concept implementation of embedding Java applets in Elgg.
+ * It includes a sample applet and game.
+ * (C) iionly 2012-2013
+ * GNU General Public License version 2
+ */
+
+return array(
+  'jpatchwork:menu' => 'JPatchwork',
+  'jpatchwork:title' => 'JPatchwork',
+  'jpatchwork:sample_title'=> 'Beispiel-Java Applet',
+  'jpatchwork:sample_menu'=> 'Beispiel',
+  'jpatchwork:frozenbubble_title'=> 'Frozen Bubble',
+  'jpatchwork:frozenbubble_menu'=> 'Frozen Bubble'
+);
